5e8b76d779affa2494a1901c1c19bd6195310a91
Timo Stollenwerk <contact@timostollenwerk.net>
2013-07-13 19:54:24 +0200

    Pep8

--- plone/recipe/codeanalysis/tests/test_docs.py
+++ plone/recipe/codeanalysis/tests/test_docs.py
@@ -11,9 +11,11 @@ import zc.buildout.testing
 
 from zope.testing import renormalizing
 
-optionflags =  (doctest.ELLIPSIS |
-                doctest.NORMALIZE_WHITESPACE |
-                doctest.REPORT_ONLY_FIRST_FAILURE)
+optionflags = (
+    doctest.ELLIPSIS |
+    doctest.NORMALIZE_WHITESPACE |
+    doctest.REPORT_ONLY_FIRST_FAILURE
+)
 
 def setUp(test):
     zc.buildout.testing.buildoutSetUp(test)
@@ -26,22 +28,22 @@ def setUp(test):
 
 def test_suite():
     suite = unittest.TestSuite((
-            doctest.DocFileSuite(
-                '../README.txt',
-                setUp=setUp,
-                tearDown=zc.buildout.testing.buildoutTearDown,
-                optionflags=optionflags,
-                checker=renormalizing.RENormalizing([
-                        # If want to clean up the doctest output you
-                        # can register additional regexp normalizers
-                        # here. The format is a two-tuple with the RE
-                        # as the first item and the replacement as the
-                        # second item, e.g.
-                        # (re.compile('my-[rR]eg[eE]ps'), 'my-regexps')
-                        zc.buildout.testing.normalize_path,
-                        ]),
-                ),
-            ))
+        doctest.DocFileSuite(
+            '../README.txt',
+            setUp=setUp,
+            tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=optionflags,
+            checker=renormalizing.RENormalizing([
+                # If want to clean up the doctest output you
+                # can register additional regexp normalizers
+                # here. The format is a two-tuple with the RE
+                # as the first item and the replacement as the
+                # second item, e.g.
+                # (re.compile('my-[rR]eg[eE]ps'), 'my-regexps')
+                zc.buildout.testing.normalize_path,
+            ]),
+        ),
+    ))
     return suite
 
 if __name__ == '__main__':
