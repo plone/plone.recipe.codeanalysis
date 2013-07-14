1c5955882a633629684af72d0d56e8e8b7560920
Timo Stollenwerk <contact@timostollenwerk.net>
2013-07-14 18:11:43 +0200

    Set version number to 1.0a1.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0'
+version = '1.0a1'
 
 long_description = (
     read('README.txt')
