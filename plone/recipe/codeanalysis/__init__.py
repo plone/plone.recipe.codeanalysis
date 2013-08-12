a609d774903e04e0885266661a68cf48e762f621
Timo Stollenwerk <contact@timostollenwerk.net>
2013-08-12 12:59:58 +0200

    Factor out jshint_code_analysis.

--- plone/recipe/codeanalysis/__init__.py
+++ plone/recipe/codeanalysis/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Recipe codeanalysis"""
 from plone.recipe.codeanalysis.flake8 import code_analysis_flake8
+from plone.recipe.codeanalysis.jshint import code_analysis_jshint
 
 import os
 import re
@@ -236,40 +237,6 @@ def code_analysis(options):
     if 'debug-statements' in options and \
             options['debug-statements'] != 'False':
         code_analysis_debug_statements(options)
-
-
-def jshint_errors(output):
-    """JSHint always return an exit code of 2 either if a file has errors or
-    warnings. This method search for markers of errors (E000).
-    """
-    error = re.compile(r'(E\d\d\d)')
-    return error.search(output)
-
-
-def code_analysis_jshint(options):
-    sys.stdout.write("JSHint")
-    sys.stdout.flush()
-
-    # cmd is a sequence of program arguments
-    # first argument is child program
-    cmd = [
-        options['jshint-bin'],
-        '--verbose',
-        '--exclude={0}'.format(options['jshint-exclude'] or ' '),
-        options['directory'],
-    ]
-    process = subprocess.Popen(
-        cmd,
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE
-    )
-    output, err = process.communicate()
-    if jshint_errors(output):  # HACK: workaround for JSHint limitations
-        print("           [\033[00;31m FAILURE \033[0m]")
-        print(output)
-    else:
-        print("                [\033[00;32m OK \033[0m]")
-        print(output)  # XXX: there should be warnings on the output
 
 
 def code_analysis_csslint(options):
---
+++ plone/recipe/codeanalysis/jshint.py
@@ -0,0 +1,37 @@
+import re
+import subprocess
+import sys
+
+
+def jshint_errors(output):
+    """JSHint always return an exit code of 2 either if a file has errors or
+    warnings. This method search for markers of errors (E000).
+    """
+    error = re.compile(r'(E\d\d\d)')
+    return error.search(output)
+
+
+def code_analysis_jshint(options):
+    sys.stdout.write("JSHint")
+    sys.stdout.flush()
+
+    # cmd is a sequence of program arguments
+    # first argument is child program
+    cmd = [
+        options['jshint-bin'],
+        '--verbose',
+        '--exclude={0}'.format(options['jshint-exclude'] or ' '),
+        options['directory'],
+    ]
+    process = subprocess.Popen(
+        cmd,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE
+    )
+    output, err = process.communicate()
+    if jshint_errors(output):  # HACK: workaround for JSHint limitations
+        print("           [\033[00;31m FAILURE \033[0m]")
+        print(output)
+    else:
+        print("                [\033[00;32m OK \033[0m]")
+        print(output)  # XXX: there should be warnings on the output
