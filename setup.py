fd7a9c4e3c0db7ce3ca5ec515e8a360363fc1bea
Timo Stollenwerk <tisto@plone.org>
2013-09-12 20:34:29 +0200

    Back to development.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0b3'
+version = '1.0b4dev'
 
 long_description = (
     read('README.rst')
