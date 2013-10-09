865d812567142e1572eb039f27393e01bc75d017
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-09 11:55:03 -0300

    Add unit test for jslint return conditions.

---
+++ plone/recipe/codeanalysis/tests/test_jshint.py
@@ -0,0 +1,31 @@
+# -*- utf-8 -*-
+
+import unittest
+from plone.recipe.codeanalysis.jshint import code_analysis_jshint
+from mock import MagicMock
+from mock import patch
+
+class TestJSHint(unittest.TestCase):
+    @patch('subprocess.Popen')
+    def test_analysis_should_return_false(self, mock_class):
+        mock_class().communicate = MagicMock(
+            return_value=(' x (E000) x ', 'IGNORED ERR',)
+        )
+        from subprocess import Popen
+        options = {'jshint-bin': 'FAKE_EXECUTABLE',
+                   'jshint-exclude': 'FAKE_EXCLUDE',
+                   'directory': 'FAKE_DIRECTORY',
+                   'jenkins': 'False'}
+        self.assertFalse(code_analysis_jshint(options))
+
+    @patch('subprocess.Popen')
+    def test_analysis_should_return_true(self, mock_class):
+        mock_class().communicate = MagicMock(
+            return_value=(' x (W000) x ', 'IGNORED ERR',)
+        )
+        from subprocess import Popen
+        options = {'jshint-bin': 'FAKE_EXECUTABLE',
+                   'jshint-exclude': 'FAKE_EXCLUDE',
+                   'directory': 'FAKE_DIRECTORY',
+                   'jenkins': 'False'}
+        self.assertTrue(code_analysis_jshint(options))
