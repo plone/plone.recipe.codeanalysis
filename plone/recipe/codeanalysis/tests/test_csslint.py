6cd6916c0b01cc5d9de049cc711920e5e00205ed
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-09 11:39:06 -0300

    Add unit test for flake8 return conditions.

--- plone/recipe/codeanalysis/tests/test_csslint.py
+++ plone/recipe/codeanalysis/tests/test_csslint.py
@@ -1,5 +1,4 @@
 # -*- utf-8 -*-
-import pdb
 
 import unittest
 from plone.recipe.codeanalysis.csslint import code_analysis_csslint
@@ -22,9 +21,8 @@ class TestCssLint(unittest.TestCase):
     @patch('subprocess.Popen')
     def test_analysis_should_return_true(self, mock_class):
         mock_class().communicate = MagicMock(
-            return_value=(' x (W000) x ', 'IGNORED ERR',)
+            return_value=(' x no error x ', 'IGNORED ERR',)
         )
-        # import pdb; pdb.set_trace()
         from subprocess import Popen
         options = {'csslint-bin': 'FAKE_EXECUTABLE',
                    'directory': 'FAKE_DIRECTORY',
---
+++ plone/recipe/codeanalysis/tests/test_flake8.py
@@ -0,0 +1,47 @@
+# -*- utf-8 -*-
+import unittest
+from plone.recipe.codeanalysis.flake8 import code_analysis_flake8
+from mock import MagicMock
+from mock import patch
+import mock
+from mock import PropertyMock
+
+
+class TestFlake8(unittest.TestCase):
+    @patch('subprocess.Popen')
+    def test_analysis_should_return_false(self, mock_class):
+        mock_class().communicate = MagicMock(
+            return_value=(' x Error should return false x ', 'IGNORED ERR',)
+        )
+        returncode = PropertyMock(return_value=1)
+        type(mock_class()).returncode = returncode
+        from subprocess import Popen
+        options = {
+            'bin-directory': 'FAKE_BIN_DIRECTORY',
+            'flake8-ignore': 'FAKE_IGNORE',
+            'flake8-exclude': 'FAKE_EXCLUDE',
+            'flake8-max-complexity': 'FAKE_MAX_COMPLEXITY',
+            'flake8-max-line-length': 'FAKE_MAX_LENGTH',
+            'directory': 'FAKE_DIRECTORY',
+            'jenkins': 'False',
+        }
+        self.assertFalse(code_analysis_flake8(options))
+
+    @patch('subprocess.Popen')
+    def test_analysis_should_return_true(self, mock_class):
+        mock_class().communicate = MagicMock(
+            return_value=(' x ok, should return true x ', 'IGNORED ERR',)
+        )
+        returncode = PropertyMock(return_value=0)
+        type(mock_class()).returncode = returncode
+        from subprocess import Popen
+        options = {
+            'bin-directory': 'FAKE_BIN_DIRECTORY',
+            'flake8-ignore': 'FAKE_IGNORE',
+            'flake8-exclude': 'FAKE_EXCLUDE',
+            'flake8-max-complexity': 'FAKE_MAX_COMPLEXITY',
+            'flake8-max-line-length': 'FAKE_MAX_LENGTH',
+            'directory': 'FAKE_DIRECTORY',
+            'jenkins': 'False',
+        }
+        self.assertTrue(code_analysis_flake8(options))
