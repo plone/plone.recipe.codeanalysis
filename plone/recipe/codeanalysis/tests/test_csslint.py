cb968fa9657cc59e7471490519a410effd957269
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-10 18:32:03 -0300

    Add test for csslint to check if the OSError exception is raised.

--- plone/recipe/codeanalysis/tests/test_csslint.py
+++ plone/recipe/codeanalysis/tests/test_csslint.py
@@ -18,6 +18,18 @@ class TestCssLint(unittest.TestCase):
         self.assertFalse(code_analysis_csslint(options))
 
     @patch('subprocess.Popen')
+    def test_analysis_should_raise_oserror(self, mock_class):
+        mock_class().communicate = MagicMock(
+            side_effect=OSError(),
+            return_value=(' x Error - x ', 'IGNORED ERR',)
+        )
+        from subprocess import Popen  # noqa
+        options = {'csslint-bin': 'FAKE_EXECUTABLE',
+                   'directory': 'FAKE_DIRECTORY',
+                   'jenkins': 'False'}
+        self.assertRaises(OSError, code_analysis_csslint, options)
+
+    @patch('subprocess.Popen')
     def test_analysis_should_return_true(self, mock_class):
         mock_class().communicate = MagicMock(
             return_value=(' x no error x ', 'IGNORED ERR',)
