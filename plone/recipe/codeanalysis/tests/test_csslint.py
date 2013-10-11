25b2825bb54e7df9fe76150247f6e763f571cedb
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-11 06:09:29 -0300

    Add test for the jenkins option.

--- plone/recipe/codeanalysis/tests/test_csslint.py
+++ plone/recipe/codeanalysis/tests/test_csslint.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-import unittest
+from unittest import TestCase
 from plone.recipe.codeanalysis.csslint import code_analysis_csslint
 from mock import MagicMock
 from mock import patch
+from shutil import rmtree
+from tempfile import mkdtemp
+from os.path import join as path_join
+from os.path import isfile as path_isfile
 
 
-class TestCssLint(unittest.TestCase):
+class TestCssLint(TestCase):
     def test_analysis_should_return_false_when_exception_occurs(self):
         options = {'csslint-bin': 'do_not_exist',
                    'directory': 'do_not_exist',
@@ -22,6 +26,22 @@ class TestCssLint(unittest.TestCase):
                    'directory': 'FAKE_DIRECTORY',
                    'jenkins': 'False'}
         self.assertFalse(code_analysis_csslint(options))
+
+    @patch('subprocess.Popen')
+    def test_analysis_file_should_exist_when_jenkins_is_true(self, mock_class):
+        tmp_dir = mkdtemp()
+        mock_class().communicate = MagicMock(
+            return_value=(' x Error - x ', 'IGNORED ERR',)
+        )
+        from subprocess import Popen  # noqa
+        options = {'csslint-bin': 'FAKE_EXECUTABLE',
+                   'directory': 'FAKE_DIRECTORY',
+                   'location': tmp_dir,
+                   'jenkins': 'True'}
+        code_analysis_csslint(options)
+        result = path_isfile(path_join(tmp_dir, 'csslint.xml'))
+        rmtree(tmp_dir)
+        self.assertTrue(result)
 
     @patch('subprocess.Popen')
     def test_analysis_should_return_true(self, mock_class):
