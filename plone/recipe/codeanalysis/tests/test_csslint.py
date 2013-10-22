e2b9ecf1c5a20c0b5a3ecf12feb401084c3cbfad
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-22 17:03:33 -0200

    Add test for csslint errors function.

--- plone/recipe/codeanalysis/tests/test_csslint.py
+++ plone/recipe/codeanalysis/tests/test_csslint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 from plone.recipe.codeanalysis.csslint import code_analysis_csslint
+from plone.recipe.codeanalysis.csslint import csslint_errors
 from shutil import rmtree
 from tempfile import mkdtemp
 from os.path import join as path_join
@@ -62,3 +63,26 @@ class TestCssLint(TestCase):
         file_exist = path_isfile(path_join(location_tmp_dir, 'csslint.xml'))
         rmtree(location_tmp_dir)
         self.assertTrue(file_exist)
+
+    def test_jshint_errors_should_return_false_xml_output(self):
+        output = '<?xml version="1.0" encoding="utf-8"?><lint>\n</lint>\n'
+        self.assertFalse(csslint_errors(output, True))
+
+    def test_jshint_errors_should_return_true_xml_output(self):
+        output = '<?xml version="1.0" encoding="utf-8"?><lint>\n<file name='\
+            '"incorrect.css"><issue line="2" char="1" severity="error" '\
+            'reason="Unexpected token \'{\' at line 2, col 1." evidence='\
+            '"{}"/><issue line="2" char="2" severity="error" '\
+            'reason="Unexpected token \'}\' at line 2, col 2." evidence='\
+            '"{}"/></file>\n</lint>\n'
+        self.assertTrue(csslint_errors(output, True))
+
+    def test_jshint_errors_should_return_true_normal_output(self):
+        output = 'incorrect.css: line 2, col 1, Error - Unexpected token '\
+            '\'{\' at line 2, col 1.\nincorrect.css: line 2, col 2, Error'\
+            ' - Unexpected token \'}\' at line 2, col 2.\n\n'
+        self.assertTrue(csslint_errors(output, False))
+
+    def test_jshint_errors_should_return_false_normal_output(self):
+        output = 'correct.css: Lint Free!\n'
+        self.assertFalse(csslint_errors(output, False))
