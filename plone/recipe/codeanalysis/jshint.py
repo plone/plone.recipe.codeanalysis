8621fe25f64ab435a4a7100e2ad09d25971dd6c8
Timo Stollenwerk <contact@timostollenwerk.net>
2013-10-24 10:33:33 +0200

    Do not print out jshint output for Jenkins.

--- plone/recipe/codeanalysis/jshint.py
+++ plone/recipe/codeanalysis/jshint.py
@@ -51,13 +51,15 @@ def code_analysis_jshint(options):
 
     # HACK: workaround for JSHint limitations
     if jshint_errors(output, jenkins):
-        log('failure')
-        # Name the pattern to use it in the substitution.
-        old, new = '\((?P<name>E\d\d\d)\)', u'(\033[00;31m\g<name>\033[0m)'
-        print _process_output(output, old, new)
+        if jenkins:
+            log('failure', 'Output file written to %s.' % output_file_name)
+        else:
+            # Name the pattern to use it in the substitution.
+            old, new = '\((?P<name>E\d\d\d)\)', u'(\033[00;31m\g<name>\033[0m)'
+            log('failure', _process_output(output, old, new))
         return False
     else:
         log('ok')
-        if output != '':
+        if output != '' and not jenkins:
             print(output)  # XXX: there should be warnings on the output
         return True
