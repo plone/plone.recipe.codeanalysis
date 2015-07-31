e517f4fd15ae8ac9a1f89a4278837d60449e6ea6
Gil Forcada <gforcada@gnome.org>
2015-07-31 10:22:28 +0200

    Typo

--- plone/recipe/codeanalysis/tests/test_jshint.py
+++ plone/recipe/codeanalysis/tests/test_jshint.py
@@ -5,7 +5,7 @@ from os.path import isfile as path_isfile
 from os.path import join as path_join
 from plone.recipe.codeanalysis.jshint import JSHint
 from plone.recipe.codeanalysis.jshint import console_script
-gifrom plone.recipe.codeanalysis.testing import CodeAnalysisTestCase
+from plone.recipe.codeanalysis.testing import CodeAnalysisTestCase
 from shutil import rmtree
 from tempfile import TemporaryFile
 from tempfile import mkdtemp
