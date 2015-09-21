505c4a3da39591dc387eeaae1e9454bb4bd0d5b1
Jan-Jaap Driessen <jdriessen@minddistrict.com>
2015-09-21 22:44:42 +0200

    chameleon lint first version.

---
+++ plone/recipe/codeanalysis/chameleonlint.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from lxml.etree import parse
+from lxml.etree import XMLSyntaxError
+from plone.recipe.codeanalysis.analyser import Analyser
+from plone.recipe.codeanalysis.analyser import console_factory
+
+import cStringIO
+import re
+
+
+# inspired by p01.checker:
+#   http://pydoc.net/Python/p01.checker/0.5.6/p01.checker.checker/
+
+DOCTYPE_WRAPPER = '''\
+<!DOCTYPE html [<!ENTITY nbsp 'no-break space'>]>
+{}'''
+
+
+class ChameleonLint(Analyser):
+
+    name = 'chameleon-lint'
+    title = 'Chameleon Lint'
+
+    def cmd(self):
+        # Please the ABC by faux-implementing the cmd.
+        pass
+
+    def run(self):
+        total_errors = []
+        files = self.find_files('.*\.cpt')
+
+        for file_path in files:
+            file_content = open(file_path, 'r').read()
+            offset = 0
+            if '<!DOCTYPE' not in file_content:
+                file_content = DOCTYPE_WRAPPER.format(file_content)
+                offset = len(DOCTYPE_WRAPPER.splitlines()) - 1
+            try:
+                parse(cStringIO.StringIO(file_content))
+            except XMLSyntaxError as e:
+                # Line number offset correction.
+                msg = e.message
+                for line_number in re.findall('line ([0-9]+)', msg):
+                    msg = msg.replace(
+                        'line {}'.format(line_number),
+                        'line {}'.format(int(line_number) - offset))
+
+                total_errors.append('{}: {}'.format(file_path, msg))
+
+        with self.open_output_file() as output_file:
+            output_file.write('\n'.join(total_errors))
+            self.parse_output(output_file, bool(total_errors))
+
+
+def console_script(options):
+    console_factory(ChameleonLint, options)
