a8a57f8812849d44f407a41415a965274b78fe75
Guido A.J. Stevens <guido.stevens@cosent.nl>
2018-02-15 15:22:22 +0000

    Widen the extensions ``i18ndude find-untranslated`` looks for

--- plone/recipe/codeanalysis/i18ndude.py
+++ plone/recipe/codeanalysis/i18ndude.py
@@ -7,11 +7,14 @@ class I18NDude(Analyser):
 
     name = 'find-untranslated'
     title = 'Translations'
+    extensions = ('pt', 'cpt', 'zpt', )
 
     @property
     def cmd(self):
         cmd = []
-        files = self.find_files('.*\.pt')
+        files = []
+        for extension in self.extensions:
+            files.extend(self.find_files('.*\.{0}'.format(extension)))
 
         if files:
             cmd.append(self.options.get('i18ndude-bin') or '')
