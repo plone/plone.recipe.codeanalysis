6f94abfb26422bf4fda6af5c7b4a2cac01f6419f
Gil Forcada Codinachs <gil.gnome@gmail.com>
2018-07-20 15:45:32 +0200

    z3c.dependencychecker use exit code
    Since version 2.3
    
    There's a ``--exit-zero`` command line switch, but we are probably not interested in using it.

--- plone/recipe/codeanalysis/dependencychecker.py
+++ plone/recipe/codeanalysis/dependencychecker.py
@@ -13,15 +13,6 @@ class DependencyChecker(Analyser):
         cmd = [self.get_prefixed_option('bin') or '']
         return cmd
 
-    def parse_output(self, output_file, return_code):
-        """dependencychecker always returns 0 even when
-        errors are found. Catch that here.
-        """
-        has_errors = bool(output_file.read())
-        output_file.seek(0)  # reset even though super does so as well
-        return super(DependencyChecker, self).parse_output(
-            output_file, has_errors)
-
 
 def console_script(options):
     console_factory(DependencyChecker, options)
