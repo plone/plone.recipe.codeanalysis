32241f7f58a3dd09b548ad9e550a028566da563e
Jan-Jaap Driessen <jdriessen@minddistrict.com>
2018-08-09 10:04:11 +0200

    Un-comment commented test cases, there seems to be nothing wrong with them

--- plone/recipe/codeanalysis/tests/test_a11y_lint.py
+++ plone/recipe/codeanalysis/tests/test_a11y_lint.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.recipe.codeanalysis.a11y import A11yLint
+from plone.recipe.codeanalysis.a11y import console_script
 from plone.recipe.codeanalysis.testing import CodeAnalysisTestCase
+from shutil import rmtree
+from tempfile import mkdtemp
 from testfixtures import OutputCapture
+
+import os
 
 
 LINK_MISSING_HREF = """\
@@ -293,26 +298,27 @@ class TestA11yLint(CodeAnalysisTestCase):
         with OutputCapture():
             self.assertTrue(A11yLint(self.options).run())
 
-    # def test_analysis_file_should_exist_when_jenkins_is_true(self):
-    #     self.given_a_file_in_test_dir('valid.pt', VALID_CODE)
-    #     parts_dir = mkdtemp()
-    #     self.options['location'] = parts_dir
-    #     self.options['jenkins'] = 'True'  # need to activate jenkins.
-    #     with OutputCapture():
-    #         A11yLint(self.options).run()
-    #     file_exists = os.path.isfile(
-    #         os.path.join(parts_dir, 'a11y-lint.log'))
-    #     rmtree(parts_dir)
-    #     self.assertTrue(file_exists)
+    def test_analysis_file_should_exist_when_jenkins_is_true(self):
+        self.given_a_file_in_test_dir('valid.pt', LINK_HREF)
+        parts_dir = mkdtemp()
+        self.options['location'] = parts_dir
+        self.options['jenkins'] = 'True'  # need to activate jenkins.
+        with OutputCapture():
+            A11yLint(self.options).run()
 
-    # def test_analysis_should_raise_systemexit_0_in_console_script(self):
-    #     self.given_a_file_in_test_dir('valid.pt', VALID_CODE)
-    #     with OutputCapture():
-    #         with self.assertRaisesRegexp(SystemExit, '0'):
-    #             console_script(self.options)
-    #
-    # def test_analysis_should_raise_systemexit_1_in_console_script(self):
-    #     self.given_a_file_in_test_dir('invalid.pt', INVALID_CODE)
-    #     with OutputCapture():
-    #         with self.assertRaisesRegexp(SystemExit, '1'):
-    #             console_script(self.options)
+        file_exists = os.path.isfile(
+            os.path.join(parts_dir, 'a11y-lint.log'))
+        rmtree(parts_dir)
+        self.assertTrue(file_exists)
+
+    def test_analysis_should_raise_systemexit_0_in_console_script(self):
+        self.given_a_file_in_test_dir('valid.pt', LINK_HREF)
+        with OutputCapture():
+            with self.assertRaisesRegexp(SystemExit, '0'):
+                console_script(self.options)
+
+    def test_analysis_should_raise_systemexit_1_in_console_script(self):
+        self.given_a_file_in_test_dir('invalid.pt', LINK_MISSING_HREF)
+        with OutputCapture():
+            with self.assertRaisesRegexp(SystemExit, '1'):
+                console_script(self.options)
